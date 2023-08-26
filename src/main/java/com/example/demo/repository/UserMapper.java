623f272602aa54808f895fa799fbbed4210ee4aa
高杉宗汰 <sotatk25@KetsuzeiMacBook-Air.local>
2023-08-26 14:37:05 +0900

    mapper修正

--- src/main/java/com/example/demo/repository/UserMapper.java
+++ src/main/java/com/example/demo/repository/UserMapper.java
@@ -3,13 +3,15 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.model.Users;
 
 @Mapper
-public class UserMapper {
+public interface UserMapper {
 
 	/** ユーザー一覧取得 */
+	@Select("SELECT * FROM users")
 	public List<Users> userLists();
 
 }
