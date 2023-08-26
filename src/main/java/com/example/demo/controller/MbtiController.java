274b91da5dd0c72739ec6d82bc907ae81b9c724e
ShogoKonishi <shogo.k20001227@gmail.com>
2023-08-26 15:06:11 +0900

    controller作成

--- src/main/java/com/example/demo/controller/MbtiController.java
+++ src/main/java/com/example/demo/controller/MbtiController.java
@@ -1,35 +1,45 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Users;
+import com.example.demo.service.UserService;
 
 @Controller
 public class MbtiController {
-	
+
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/list")
 	public String getList() {
+		Map<String, List<Users>> userListMap = userService.getUerLists();
+		System.out.println(userListMap);
 		return "/list";
 	}
-	
-	
+
 	@GetMapping("/add")
 	public String getAdd() {
 		return "/add";
 	}
-	
-	@PostMapping("/add")
-	public String postAdd() {
-		return "redirect:/list";
-	}
-	
+
+	//	@PostMapping("/add")
+	//	public String postAdd() {
+	//		return "redirect:/list";
+	//	}
+
 	@GetMapping("/update")
 	public String getUpdate() {
 		return "/update";
 	}
-	
-	@PostMapping("/add")
-	public String postUpdate() {
-		return "redirect:/list";
-	}
+
+	//	@PostMapping("/add")
+	//	public String postUpdate() {
+	//		return "redirect:/list";
+	//	}
 }
