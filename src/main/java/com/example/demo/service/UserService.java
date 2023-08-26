93fbe878d04c6a53075b4698d0ebec9e84e689c4
高杉宗汰 <sotatk25@KetsuzeiMacBook-Air.local>
2023-08-26 14:51:07 +0900

    serviceクラス実装完了

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.example.demo.model.Users;
 
 public interface UserService {
 
 	/** ユーザー一覧取得 */
-	public List<Users> userLists();
+	public Map<String, List<Users>> getUerLists();
 
 }
